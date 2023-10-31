4f74ec204c39cdba9aed32b3c0aa3e92df8247bb
Maeda, Hideyoshi <hideyoshi.maeda@afiniti.com>
2023-10-31 13:21:54 +0000

    Update code with new website

--- js/cookies_basket_functions.js
+++ js/cookies_basket_functions.js
@@ -1,77 +1,67 @@
-function createBasketCookie() {
-	$(document).click(function(event){
-		var value = event.target.parentElement.parentElement.children[0].innerText.trim();
-		console.log(value);
+$('a.card-button').click(function () {
+	let value = $(this).attr('data-bundle');
+	console.log("testttttttttttttt: ", value);
+	if (value == "Big Bundle") {
+		value = value + ": 33";
+	}
 
-		if (value == "Big Bundle") {
-			value = value + ": 33";
-		}
+	if (value == "Bigger Bundle") {
+		value = value + ": 49";
+	}
+	if (value == "Bigger Bundle sports") {
+		value = value + ": 65";
+	}
 
-		if (value == "Bigger Bundle") {
-			value = value + ": 49";
-		}
-		if (value == "Bigger Bundle sports") {
-			value = value + ": 65";
-		}
+	if (value == "Ultimate Bundle") {
+		value = value + ": 89";
+	}
 
-		if (value == "Ultimate Bundle") {
-			value = value + ": 89";
-		}
+	if (value == "M100 Fibre BB Phone") {
+		value = value + ": 27";
+	}
 
-		if (value == "M100 Fibre BB Phone") {
-			value = value + ": 27";
-		}
+	if (value == "M200 Fibre BB Phone") {
+		value = value + ": 33";
+	}
 
-		if (value == "M200 Fibre BB Phone") {
-			value = value + ": 33";
-		}
+	if (value == "M350 Fibre BB Phone") {
+		value = value + ": 39";
+	}
 
-		if (value == "M350 Fibre BB Phone") {
-			value = value + ": 39";
-		}
+	if (value == "M500 Fibre BB Phone") {
+		value = value + ": 45";
+	}
 
-		if (value == "M500 Fibre BB Phone") {
-			value = value + ": 45";
-		}
+	if (value == "M100 Fibre BB") {
+		value = value + ": 44";
+	}
 
-		if (value == "M100 Fibre BB") {
-			value = value + ": 44";
-		}
+	if (value == "M350 Fibre BB") {
+		value = value + ": 56";
+	}
 
-		if (value == "M350 Fibre BB") {
-			value = value + ": 56";
-		}
+	if (value == "M500 Fibre BB") {
+		value = value + ": 62";
+	}
 
-		if (value == "M500 Fibre BB") {
-			value = value + ": 62";
-		}
-
-		if (value.includes("Gig1")) {
-			value = value + ": 62";
-		}
-                
-		setCookie("basket", value, 30);
-        console.log(getCookieValue("basket"));
-		// console.log(value);
-		// alert(value);
-
-		// alert(getCookieValue("basket"));
-	});
-}
+	if (value == "M1000 Fibre BB") {
+		value = value + ": 62";
+	}
+							
+	setCookie("basket", value, 30);
+	console.log(getCookieValue("basket"));
+})
 
 function updateBasket() {
 	var value = getCookieValue("basket");
-	console.log(value);
+	console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",value);
 
 	if (value == "") {
-		document.getElementById("basket_content_dynamic").innerHTML = "Currently Empty";
-		document.getElementById("remove_basket").style.display      = "none";
-		document.getElementById("proceed_eli").style.display        = "none";
+		$('#li-proceed').addClass('d-none');
+		$('#li-empty').removeClass('d-none');
 	} else {
-		document.getElementById("basket_content_dynamic").innerHTML = value;
-		document.getElementById("remove_basket").style.display      = "block";	
-		document.getElementById("proceed_eli").style.display        = "block";
-			
+		$('#li-proceed').removeClass('d-none');
+		$('#li-empty').addClass('d-none');
 	}
 }
 
@@ -79,7 +69,7 @@ function updateCartPrice() {
     var cvalue = getCookieValue("basket").split(":");
 
     document.getElementById("title_bundle").innerHTML   = cvalue[0];
-    document.getElementById("single_price").innerHTML   = cvalue[1].trim();
-    document.getElementById("total_price").innerHTML    = cvalue[1].trim();
+    document.getElementById("single_price").innerHTML   = '$' + cvalue[1].trim();
+    document.getElementById("total_price").innerHTML    = '$' + cvalue[1].trim();
     // document.getElementById("number_product").innerHTML = "1";
   }
